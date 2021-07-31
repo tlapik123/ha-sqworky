e5c9d9c0777d1173a51f10b95b3ace97acb58aa7
Petr Tlapa <tlapik123@gmail.com>
2021-07-31 14:55:14 +0200

    more testing

--- app/Main.hs
+++ app/Main.hs
@@ -2,14 +2,94 @@ module Main where
 
 import Graphics.Gloss
 
-window :: Display 
-window = InWindow "Good looking window" (200,200) (10,10)
+width, height, offset :: Int
+width = 300
+height = 300
+offset = 100
 
-background :: Color 
-background = white
+window :: Display
+window = InWindow "Almost Pong" (width, height) (offset, offset)
+
+background :: Color
+background = black
 
 drawing :: Picture
-drawing = rectangleSolid 80 40
+drawing = pictures [ball, walls,
+                    mkPaddle rose 120 (-20),
+                    mkPaddle orange (-120) 40]
+  where
+    --  The pong ball.
+    ball = translate (-10) 40 $ color ballColor $ circleSolid 10
+    ballColor = dark red
+
+    --  The bottom and top walls.
+    wall :: Float -> Picture
+    wall offset =
+      translate 0 offset $
+        color wallColor $
+          rectangleSolid 270 10
+
+    wallColor = greyN 0.5
+    walls = pictures [wall 150, wall (-150)]
+
+    --  Make a paddle of a given border and vertical offset.
+    mkPaddle :: Color -> Float -> Float -> Picture
+    mkPaddle col x y = pictures
+      [ translate x y $ color col $ rectangleSolid 26 86
+      , translate x y $ color paddleColor $ rectangleSolid 20 80
+      ]
+
+    paddleColor = light (light blue)
+
+
+-- | A data structure to hold the state of the Pong game.
+data PongGame = Game {
+    ballLoc :: (Float, Float),  -- ^ Pong ball (x, y) location.
+    ballVel :: (Float, Float),  -- ^ Pong ball (x, y) velocity. 
+    player1 :: Float,           -- ^ Left player paddle height.
+                                -- Zero is the middle of the screen.
+    player2 :: Float
+} deriving Show
+
+-- | Convert a game state into a picture.
+render :: PongGame  -- ^ The game state to render.
+       -> Picture   -- ^ A picture of this game state.
+render game =
+  pictures [ball, walls,
+            mkPaddle rose 120 $ player1 game,
+            mkPaddle orange (-120) $ player2 game]
+  where
+    --  The pong ball.
+    ball = uncurry translate (ballLoc game) $ color ballColor $ circleSolid 10
+    ballColor = dark red
+
+    --  The bottom and top walls.
+    wall :: Float -> Picture
+    wall offset =
+      translate 0 offset $
+        color wallColor $
+          rectangleSolid 270 10
+
+    wallColor = greyN 0.5
+    walls = pictures [wall 150, wall (-150)]
+
+    --  Make a paddle of a given border and vertical offset.
+    mkPaddle :: Color -> Float -> Float -> Picture
+    mkPaddle col x y = pictures
+      [ translate x y $ color col $ rectangleSolid 26 86
+      , translate x y $ color paddleColor $ rectangleSolid 20 80
+      ]
+
+    paddleColor = light (light blue)
+
+-- | Initialize the game with this game state.
+initialState :: PongGame
+initialState = Game {
+    ballLoc = (-10, 30),
+    ballVel = (1, -3),
+    player1 = 40,
+    player2 = -80
+}
 
 main :: IO ()
 main = display window background drawing
